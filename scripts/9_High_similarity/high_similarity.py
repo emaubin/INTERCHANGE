a44cc1865fc9f2697c7061b7bd73215ddf9132e3
Emilie Aubin <48484621+emaubin@users.noreply.github.com>
2023-04-11 17:45:34 +0200

    Update high_similarity.py

--- scripts/9_High_similarity/high_similarity.py
+++ scripts/9_High_similarity/high_similarity.py
@@ -89,14 +89,16 @@ threads=param[3]
 comparisons=param[4]
 output_blast=param[5]
 blast_path=param[6]
-
+Gene_annot=param[7]
+TE_annot=param[8]
+Unknown_annot=param[9]
 
 reciprocal=Blast()
 reciprocal.makeblastdb(db,blast_path)
 reciprocal.blastn(db,query,output,threads,blast_path)
 
 ###########################################################################################################################################
-###########################################################################################################################################
+############################################## Calculation of HS ##########################################################################
 ###########################################################################################################################################
 
 
@@ -109,6 +111,7 @@ with open(comparisons,'r') as filin:
         spindex=line[0]
         if spindex not in list_specie1:
             list_specie1.append(spindex)
+    filin.close()
 
 list_file=[]
 i=0
@@ -121,24 +124,20 @@ while i<len(list_specie1):
             outputhit=f"{output_blast}/{list_specie1[i]}_{list_specie1[d]}_busco_genes.txt"
             if outputhit not in list_file:
                 list_file.append(outputhit)
-            print(blast1)
-            print(blast2)
-            print(outputhit)
-            print(f"{list_specie1[i]}\t{list_specie1[d]}")
-            print(f"{i} and {d}")
             my_analysis=ComparisonBlast()
             my_analysis.besthit(blast1,blast2,outputhit)
         d+=1
     i+=1
 
-print(list_file)
+#print(list_file)
 
 
 list_pid=[]
 
+hs_file = open(f"{output_blast}/Busco_genes_HS.txt", 'a')
 for file in list_file:
     splt=file.split("/")
-    species=splt[7]
+    species=splt[3]
     splt2=species.split("_")
     sp1=splt2[0]
     sp2=splt2[1]
@@ -158,17 +157,182 @@ for file in list_file:
     Q3=np.percentile(list_pid, 75, interpolation = 'midpoint')
     IQR = Q3 - Q1
     HS = Q3 + (IQR / 2)
-    hs_file = open(f"{output_blast}/Busco_genes_HS_{sp1}_{sp2}.txt", 'w')
     hs_file.write(f"{sp1}\t{sp2}\t{Q1}\t{Q3}\t{HS}\n")
 
+hs_file.close()
 
-os.system(f"cat {output_blast}/Busco_genes_HS_*.txt")
+###############################################################################################################
+######################################### Building HS dictionnary #############################################
+###############################################################################################################
 
-###########################################################################################################################################
-###########################################################################################################################################
-###########################################################################################################################################
+HS_dict={}
+with open(f'{output_blast}/Busco_genes_HS.txt', 'r') as filin:
+    for line in filin:
+        line=line[:-1]
+        line=line.split('\t')
+        sp1=line[0]
+        sp2=line[1]
+        sp12=f"{sp1}-{sp2}"
+        sp21=f"{sp2}-{sp1}"
+        HS=float(line[4])
+        if sp12 not in HS_dict:
+            HS_dict[sp12]=HS
+        if sp21 not in HS_dict:
+            HS_dict[sp21]=HS
+    filin.close()
 
-end=time.time()
-time_sec=end-start
-time_hour=str(datetime.timedelta(seconds=time_sec))
-print(f"Job execution time : {time_hour}")
+#print(HS_dict)
+
+###############################################################################################################
+######################################### Selection of genes validating HS ####################################
+###############################################################################################################
+
+Gene_HS=open(f"{Gene_annot}.temp", 'w')
+with open(f"{Gene_annot}", 'r') as filin:
+    for line in filin:
+        line=line[:-1]
+        line=line.split("\t")
+        sp1=line[0]
+        sp2=line[1]
+        sp=f"{sp1}-{sp2}"
+        gene=line[2]
+        PID=float(line[3])
+        sp1_id_reads=line[4]
+        sp2_id_reads=line[5]
+        if sp in HS_dict.keys():
+            Gene_HS.write(f"{sp1}\t{sp2}\t{gene}\t{PID}\t{sp1_id_reads}\t{sp2_id_reads}\t{HS_dict.get(sp)}\n")
+    filin.close()
+Gene_HS.close()
+
+Gene_HSvalidation=open(f"{Gene_annot}_HSvalidated",'w')
+with open(f"{Gene_annot}.temp", "r") as filin:
+    for line in filin:
+        line = line [:-1]
+        line = line.split("\t")
+        sp1=line[0]
+        sp2=line[1]
+        gene=line[2]
+        PID=float(line[3])
+        sp1_id_reads=line[4]
+        sp2_id_reads=line[5]
+        HS=float(line[6])
+        if PID >= HS:
+            print(f"{gene} validates HS")
+            Gene_HSvalidation.write(f"{sp1}\t{sp2}\t{gene}\t{PID}\t{sp1_id_reads}\t{sp2_id_reads}\t{HS}\n")
+        else:
+            print(f"{gene} not validates HS")
+    filin.close()
+
+Gene_HSvalidation.close()
+
+###############################################################################################################
+######################################### Selection of TEs validating HS ######################################
+###############################################################################################################
+
+TE_HS=open(f"{TE_annot}.temp", 'w')
+with open(f"{TE_annot}", 'r') as filin:
+    for line in filin:
+        line=line[:-1]
+        line=line.split("\t")
+        sp1=line[0]
+        sp2=line[1]
+        sp=f"{sp1}-{sp2}"
+        te=line[2]
+        PID=float(line[3])
+        sp1_id_reads=line[4]
+        sp2_id_reads=line[5]
+        if sp in HS_dict.keys():
+            Gene_HS.write(f"{sp1}\t{sp2}\t{te}\t{PID}\t{sp1_id_reads}\t{sp2_id_reads}\t{HS_dict.get(sp)}\n")
+    filin.close()
+TE_HS.close()
+
+TE_HSvalidation=open(f"{TE_annot}_HSvalidated",'w')
+with open(f"{TE_annot}.temp", "r") as filin:
+    for line in filin:
+        line = line [:-1]
+        line = line.split("\t")
+        sp1=line[0]
+        sp2=line[1]
+        te=line[2]
+        PID=float(line[3])
+        sp1_id_reads=line[4]
+        sp2_id_reads=line[5]
+        HS=float(line[6])
+        if PID >= HS:
+            print(f"{te} validates HS")
+            Gene_HSvalidation.write(f"{sp1}\t{sp2}\t{te}\t{PID}\t{sp1_id_reads}\t{sp2_id_reads}\t{HS}\n")
+        else:
+            print(f"{te} not validates HS")
+    filin.close()
+
+TE_HSvalidation.close()
+
+
+###############################################################################################################
+######################################### Selection of Unknowns validating HS #################################
+###############################################################################################################
+
+Unknown_HS=open(f"{Unknown_annot}.temp", 'w')
+with open(f"{Unknown_annot}", 'r') as filin:
+    for line in filin:
+        line=line[:-1]
+        line=line.split("\t")
+        sp1=line[0]
+        sp2=line[1]
+        sp=f"{sp1}-{sp2}"
+        PID=float(line[3])
+        sp1_id_reads=line[4]
+        sp2_id_reads=line[5]
+        if sp in HS_dict.keys():
+            Gene_HS.write(f"{sp1}\t{sp2}\tUnknown\t{PID}\t{sp1_id_reads}\t{sp2_id_reads}\t{HS_dict.get(sp)}\n")
+    filin.close()
+Unknown_HS.close()
+
+Unknown_HSvalidation=open(f"{Unknown_annot}_HSvalidated",'w')
+with open(f"{Unknown_HSvalidation}.temp", "r") as filin:
+    for line in filin:
+        line = line [:-1]
+        line = line.split("\t")
+        sp1=line[0]
+        sp2=line[1]
+        PID=float(line[3])
+        sp1_id_reads=line[4]
+        sp2_id_reads=line[5]
+        HS=float(line[6])
+        if PID >= HS:
+            print(f"{sp1_id_reads} validates HS")
+            Gene_HSvalidation.write(f"{sp1}\t{sp2}\tUnknown\t{PID}\t{sp1_id_reads}\t{sp2_id_reads}\t{HS}\n")
+        else:
+            print(f"{sp1_id_reads} not validates HS")
+    filin.close()
+
+Unknown_HSvalidation.close()
+
+
+###############################################################################################################
+######################################### Deleting .temp files ################################################
+###############################################################################################################
+
+if os.path.isfile(f"{Gene_annot}.temp"):
+  os.remove(f"{Gene_annot}.temp")
+  print(f"{Gene_annot}.temp has been deleted")
+else:
+  print(f"{Gene_annot}.temp does not exist")
+
+if os.path.isfile(f"{TE_annot}.temp"):
+  os.remove(f"{TE_annot}.temp")
+  print(f"{TE_annot}.temp has been deleted")
+else:
+  print(f"{TE_annot}.temp does not exist")
+
+if os.path.isfile(f"{Unknown_annot}.temp"):
+  os.remove(f"{Unknown_annot}.temp")
+  print(f"{Unknown_annot}.temp has been deleted")
+else:
+  print(f"{Unknown_annot}.temp does not exist")
+
+###############################################################################################################
+###############################################################################################################
+###############################################################################################################
+
+print(f"Test of High Similarity done")
